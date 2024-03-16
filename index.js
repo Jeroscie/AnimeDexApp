63a7237ca1ea48dbb9f2decf26cd3536e7f343f1
Kakarot <107659390+Jeroscie@users.noreply.github.com>
2024-03-16 14:37:37 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // Enable CORS for all routes
+
+app.use(express.static('public'));
+
+// Proxy route for fetching upcoming anime
+app.get('/api/upcoming/:pageNumber', async (req, res) => {
+    try {
+        const pageNumber = req.params.pageNumber;
+        const apiUrl = `https://testaopi.vercel.app/api/upcoming/${pageNumber}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching upcoming anime data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Route for fetching popular anime
+app.get('/api/popular/:pageNumber', async (req, res) => {
+    try {
+        const pageNumber = req.params.pageNumber;
+        const apiUrl = `https://testaopi.vercel.app/api/popular/${pageNumber}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching popular anime data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Route for fetching recent anime
+app.get('/api/recent/:pageNumber', async (req, res) => {
+    try {
+        const pageNumber = req.params.pageNumber;
+        const apiUrl = `https://testaopi.vercel.app/api/recent/${pageNumber}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching recent anime data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Route for fetching anime by name
+app.get('/api/anime/:animeName', async (req, res) => {
+    try {
+        const animeName = req.params.animeName;
+        const apiUrl = `https://testaopi.vercel.app/api/anime/${animeName}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching anime data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Route for fetching episode data
+app.get('/api/episode/:animeName/:episodeNumber', async (req, res) => {
+    try {
+        const animeName = req.params.animeName;
+        const episodeNumber = req.params.episodeNumber;
+        const apiUrl = `https://testaopi.vercel.app/api/episode/${animeName}/${episodeNumber}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        res.header('Access-Control-Allow-Origin', '*');
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching episode data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Route for searching anime
+app.get('/api/search', async (req, res) => {
+    try {
+        const query = req.query.query;
+        const apiUrl = `https://testaopi.vercel.app/api/search?query=${encodeURIComponent(query)}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching search data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
